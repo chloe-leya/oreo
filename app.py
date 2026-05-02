3ee84f970e730dc87a185aca94beccb054503384
LIAO Leya <1610051602@qq.com>
2026-05-02 14:46:35 +0800

    Update image source in app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.write("ISOM5240")
 st.title(":snowman:")
 st.header("header")
-st.image("https://steamcommunity.com/sharedfiles/filedetails/?l=tchinese&id=2382152428")
+st.image("https://unsplash.com/photos/I2UR7wEftf4")
