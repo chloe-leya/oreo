1a4d0a109eba2e9cffae91c5aa9cc7cc75122bc6
LIAO Leya <1610051602@qq.com>
2026-05-02 14:45:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.write("ISOM5240")
 st.title(":snowman:")
 st.header("header")
-st.image("sunrise.jpg", caption="Sunrise by the mountains")
+st.image("https://huaban.com/pins/6445614966/")
